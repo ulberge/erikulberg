862f579d2f1c0539bfc08dda8f7c284e3972d2f7
Erik Ulberg <ulberge@gmail.com>
2016-09-26 09:06:07 -0700

    update images

--- app/Components/Sculpture.js
+++ app/Components/Sculpture.js
@@ -9,7 +9,6 @@ const images = [
   { src: './imgs/reliefmeandcarl.jpg', zoom: './imgs/zoom/reliefmeandcarl.jpg' },
   { src: './imgs/gloria1.jpg', zoom: './imgs/zoom/gloria1.jpg' },
   { src: './imgs/gloria2.jpg', zoom: './imgs/zoom/gloria2.jpg' },
-  { src: './imgs/6.jpg', zoom: './imgs/6.jpg' },
   { src: './imgs/7.jpg', zoom: './imgs/7.jpg' },
   { src: './imgs/1.jpg', zoom: './imgs/1.jpg' },
   { src: './imgs/5.jpg', zoom: './imgs/5.jpg' },
