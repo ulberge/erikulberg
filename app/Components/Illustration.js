b4d2a5e8dbf0367af899070588f6ed58933eabc1
Erik Ulberg <ulberge@gmail.com>
2016-10-18 15:48:13 -0700

    illustration page

--- app/Components/Illustration.js
+++ app/Components/Illustration.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classnames from 'classnames';
+import $ from 'jquery';
 
 import { className } from './Illustration.less';
 
-import Gallery from './Gallery';
-
-const images = [
-  { src: './imgs/seadragon_midsize.jpg', zoom: './imgs/zoom/seadragon.jpg' }
-];
-
-export default class extends Component {
+module.exports = React.createClass({
+  componentDidMount: function componentDidMount() {
+    $('#main').css({
+      background: 'url(../../imgs/zoom/seadragon.jpg)',
+      'background-size': '100%',
+      'min-height': '900px'
+    });
+    $('.navbar .container').css({
+      'background-color': 'rgba(255, 255, 255, 0.8)'
+    });
+    $('.navbar .container .navbar-collapse').css({
+      'padding-bottom': '2em'
+    });
+  },
+  componentWillUnmount: function componentWillUnmount() {
+    $('#main').css({
+      background: '#fff',
+      'min-height': 'none'
+    });
+    $('.navbar .container').css({
+      'background-color': 'rgba(255, 255, 255, 0.6)'
+    });
+    $('.navbar .container .navbar-collapse').css({
+      'padding-bottom': '1em'
+    });
+  },
   render() {
     return (
-        <div className={classnames(className, 'container')}>
-          <Gallery images={images}/>
+        <div className={classnames(className, 'container wallpaper')}>
         </div>
     );
   }
-}
+});
