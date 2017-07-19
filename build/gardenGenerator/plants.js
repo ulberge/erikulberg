5b593d5219447da445abd5ebb83dad292e08bab6
Erik Ulberg <ulberge@gmail.com>
2017-07-18 20:25:20 -0700

    final garden

--- build/gardenGenerator/plants.js
+++ build/gardenGenerator/plants.js
@@ -1,4 +1,140 @@
+/*
+*
+* plants.js
+* By: Erik Ulberg
+* Date:7/18/2017
+*
+* Functions for adding a random assortment of plants to the scene.
+*
+*/
+
 Plants = function () {
+  var minDistanceBetweenPlants = 70;
+  var minPlants = 40;
+  var plantVariance = 20;
+  var plantScale = 30;
+  var bedPadding = 10;
+
+  // Generate a random assortment of plants and add the plants and their shadows to the scene
+  this.generateAndAddPlants = function(plantList) {
+    var plantsToAdd = generatePlants();
+    addPlants(plantList, plantsToAdd);
+  };
+
+  // Load the obj and png files for the plants into a cache for faster regeneration of gardens
+  this.loadAllPlantFiles = function() {
+    for (var i = 0; i < PLANTS.length; i++) {
+      loadPlantFiles(PLANTS[i]);
+    }
+  };
+
+  // Given a list of scene objects to add to and a list of plants to add, add the plants and their shadows
+  function addPlants (plantList, plantsToAdd) {
+    for (var i = 0; i < plantsToAdd.length; i++) {
+      addPlant(plantList, plantsToAdd[i]);
+    }
+  };
+
+  // Generate a random assortment of plants and return them
+  function generatePlants () {
+    var plantsAdded = [];
+
+    var numPlants = Math.floor(Math.random()*plantVariance) + minPlants;
+    var numTry = 400;
+    for (var i = 0; i < numTry; i++) {
+      if (plantsAdded.length >= numPlants) {
+        break;
+      }
+
+      // Select a random plant and make copy
+      var plantIndex = Math.floor(Math.random()*PLANTS.length);
+      var newPlant = {};
+      Object.assign(newPlant, PLANTS[plantIndex]);
+
+      // Calculate random placement
+      var minX = (-planeWidth/2) + newPlant.size + bedPadding;
+      var maxX = (planeWidth/2) - newPlant.size - bedPadding;
+      var minZ = (-planeDepth/2) + newPlant.size + bedPadding;
+      var maxZ = (planeDepth/2) - newPlant.size - bedPadding;
+      var posX = (Math.random()*(maxX-minX)) + minX;
+      var posZ = (Math.random()*(maxZ-minZ)) + minZ;
+      newPlant.pos2D = new THREE.Vector3( posX, 0, posZ );
+      newPlant.pos3D = new THREE.Vector3( posX, newPlant.offsetY, posZ );
+
+      // Don't add if plant is too close to others
+      var isOverlapping = false;
+      for (var j = 0; j < plantsAdded.length; j++) {
+        var distanceBetween = plantsAdded[j].pos2D.distanceTo(newPlant.pos2D);
+        var minDistanceRequired = plantsAdded[j].size + newPlant.size;
+        if (distanceBetween < minDistanceRequired) {
+          isOverlapping = true;
+          break;
+        }
+      }
+      if (isOverlapping) {
+        continue;
+      }
+
+      plantsAdded.push(newPlant);
+    }
+
+    console.log('Plants added: ' + plantsAdded.length);
+    console.log('Plants desired: ' + numPlants);
+
+    return plantsAdded;
+  };
+
+  function loadPlantFiles (plant) {
+    Utilities.loadObjAndPng(plant.name,
+      function(texture) {
+        plant.texture = texture;
+      },
+      function(object) {
+        plant.obj = object;
+      }
+    );
+  }
+
+  function addPlant(plantList, plant) {
+    // If the plant files haven't been loaded yet for the static list, load them to prevent objects not showing up
+    if (!plant.obj || !plant.texture) {
+      Utilities.loadObjAndPng(plant.name,
+        function(texture) {},
+        function(object) {
+          addPlantObject(plantList, plant, object);
+        }
+      );
+    } else {
+      addPlantObject(plantList, plant, plant.obj.clone());
+    }
+  }
+
+  function addPlantObject(plantList, plant, object) {
+    object.position.copy(plant.pos3D);
+    if (plant.scale) {
+      object.scale.multiplyScalar( plant.scale );
+    } else {
+      // if no scale provided, use default
+      object.scale.multiplyScalar( plantScale );
+    }
+
+    scene.add(object);
+    plantList.push(object);
+
+    // Draw shadow circle
+    var geometry = new THREE.CircleGeometry( plant.size, 32 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.3, depthWrite: false } );
+    var circle = new THREE.Mesh( geometry, material );
+    circle.position.x = plant.pos3D.x;
+    circle.position.y = 0.1;
+    circle.position.z = plant.pos3D.z;
+    circle.rotation.x = -Math.PI/2;
+
+    scene.add(circle);
+    plantList.push(circle);
+  }
+
+  // Static list of plants to draw from. Scale is for scaling the object file, size is for their size in the world
   var PLANTS = [
     {
       name: 'obj/plants/echinacea_text',
@@ -8,7 +144,8 @@ Plants = function () {
     {
       name: 'obj/plants/redflower',
       offsetY: 10,
-      size: 1 * plantScale
+      size: 1 * plantScale,
+      scale: plantScale*0.8
     },
     {
       name: 'obj/plants/rodgersia',
@@ -74,187 +211,5 @@ Plants = function () {
       size: 1.5 * plantScale
     },
   ];
-
-  this.loadAllPlantFiles = function() {
-    for (var i = 0; i < PLANTS.length; i++) {
-      loadPlantFiles(PLANTS[i]);
-    }
-  };
-
-  this.generatePlants = function (plantList) {
-    var plantsAdded = [];
-
-    var numPlants = Math.floor(Math.random()*plantVariance) + minPlants;
-    var numTry = 400;
-    for (var i = 0; i < numTry; i++) {
-      if (plantsAdded.length >= numPlants) {
-        break;
-      }
-
-      // Select a random plant and make copy
-      var plantIndex = Math.floor(Math.random()*PLANTS.length);
-      var newPlant = {};
-      Object.assign(newPlant, PLANTS[plantIndex]);
-
-      // Calculate random placement
-      var minX = (-planeWidth/2) + newPlant.size + bedPadding;
-      var maxX = (planeWidth/2) - newPlant.size - bedPadding;
-      var minZ = (-planeDepth/2) + newPlant.size + bedPadding;
-      var maxZ = (planeDepth/2) - newPlant.size - bedPadding;
-      var posX = (Math.random()*(maxX-minX)) + minX;
-      var posZ = (Math.random()*(maxZ-minZ)) + minZ;
-      newPlant.pos2D = new THREE.Vector3( posX, 0, posZ );
-      newPlant.pos3D = new THREE.Vector3( posX, newPlant.offsetY, posZ );
-
-      // Don't add if plant is too close to others
-      var isOverlapping = false;
-      for (var j = 0; j < plantsAdded.length; j++) {
-        var distanceBetween = plantsAdded[j].pos2D.distanceTo(newPlant.pos2D);
-        var minDistanceRequired = plantsAdded[j].size + newPlant.size;
-        if (distanceBetween < minDistanceRequired) {
-          isOverlapping = true;
-          break;
-        }
-      }
-      if (isOverlapping) {
-        continue;
-      }
-
-      // Add this plant
-      plantsAdded.push(newPlant);
-
-      addPlant(plantList, newPlant, newPlant.pos3D, new THREE.Vector3(0,0,0), newPlant.scale);
-    }
-    console.log('Plants added: ' + plantsAdded.length);
-    console.log('Plants desired: ' + numPlants);
-  };
-
-  function loadPlantFiles (currentPlant) {
-      // texture
-    var manager = new THREE.LoadingManager();
-    manager.onProgress = function ( item, loaded, total ) {
-      console.log( item, loaded, total );
-    };
-
-    var texture = new THREE.Texture();
-    var loader = new THREE.ImageLoader( manager );
-    loader.load( currentPlant.name + '.png', function ( image ) {
-
-      texture.image = image;
-      texture.needsUpdate = true;
-      texture.minFilter = THREE.LinearFilter;
-      currentPlant.texture = texture;
-    } );
-
-    var onProgress = function ( xhr ) {
-      if ( xhr.lengthComputable ) {
-        var percentComplete = xhr.loaded / xhr.total * 100;
-        console.log( Math.round(percentComplete, 2) + '% downloaded' );
-      }
-    };
-
-    var onError = function ( xhr ) {
-    };
-    
-    // model
-    var loader = new THREE.OBJLoader( manager );
-    loader.load( currentPlant.name + '.obj', function ( object ) {
-
-      object.traverse( function ( child ) {
-        if ( child instanceof THREE.Mesh ) {
-          child.material.side = THREE.DoubleSide;
-          child.material.map = texture;
-        }
-
-      } );
-
-      currentPlant.obj = object;
-
-    }, onProgress, onError );
-  }
-
-  function addPlant(plantList, plant, position, rotation, scale) {
-    // texture
-    var manager = new THREE.LoadingManager();
-    manager.onProgress = function ( item, loaded, total ) {
-      console.log( item, loaded, total );
-    };
-
-    var texture;
-    if (!plant.texture) {
-      texture = new THREE.Texture();
-      var loader = new THREE.ImageLoader( manager );
-      loader.load( plant.name + '.png', function ( image ) {
-
-        texture.image = image;
-        texture.needsUpdate = true;
-        texture.minFilter = THREE.LinearFilter;
-
-      } );
-    } else {
-      texture = plant.texture;
-    }
-
-    if (!plant.obj) {
-      var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
-          var percentComplete = xhr.loaded / xhr.total * 100;
-          console.log( Math.round(percentComplete, 2) + '% downloaded' );
-        }
-      };
-
-      var onError = function ( xhr ) {
-      };
-      
-      // model
-      var loader = new THREE.OBJLoader( manager );
-      loader.load( plant.name + '.obj', function ( object ) {
-
-        object.traverse( function ( child ) {
-          if ( child instanceof THREE.Mesh ) {
-            child.material.side = THREE.DoubleSide;
-            child.material.map = texture;
-          }
-
-        } );
-
-        addPlantObject(plantList, plant, object, position, rotation, scale);
-
-      }, onProgress, onError );
-    } else {
-      addPlantObject(plantList, plant, plant.obj.clone(), position, rotation, scale);
-    }
-  }
-
-  function addPlantObject(plantList, plant, object, position, rotation, scale) {
-    object.position.set( location.x, location.y, location.z );
-    if (rotation) {
-      object.rotation.y = rotation.y;
-    }
-    if (scale) {
-      object.scale.multiplyScalar( scale );
-    } else {
-      object.scale.multiplyScalar( plantScale );
-    }
-
-    object.position.x = position.x;
-    object.position.y = position.y;
-    object.position.z = position.z;
-
-    var geometry = new THREE.CircleGeometry( plant.size, 32 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.3, depthWrite: false } );
-    var circle = new THREE.Mesh( geometry, material );
-    circle.position.x = position.x;
-    circle.position.y = 0.1;
-    circle.position.z = position.z;
-    circle.rotation.x = -Math.PI/2;
-
-
-    scene.add( circle );
-    plantList.push(circle);
-
-    scene.add( object );
-    plantList.push(object);
-  }
 };
 
--- build/gardenGenerator/utilities.js
+++ build/gardenGenerator/utilities.js
@@ -1,33 +1,50 @@
-function buildAxes( length ) {
-  var axes = new THREE.Object3D();
+/*
+*
+* utilities.js
+* By: Erik Ulberg
+* Date:7/18/2017
+*
+*/
 
-  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) ); // +X
-  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xFF0000, true) ); // -X
-  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x00FF00, false ) ); // +Y
-  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x00FF00, true ) ); // -Y
-  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0x0000FF, false ) ); // +Z
-  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0x0000FF, true ) ); // -Z
+var Utilities = {
 
-  return axes;
+  loadObjAndPng: function (filename, textureCallback, objectCallback) {
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+      console.log( item, loaded, total );
+    };
 
-}
+    var onProgress = function ( xhr ) {};
+    var onError = function ( xhr ) {};
+    var texture = new THREE.Texture();
+    var loader = new THREE.ImageLoader( manager );
+    loader.load( filename + '.png', function ( image ) {
 
-function buildAxis( src, dst, colorHex, dashed ) {
-  var geom = new THREE.Geometry(),
-      mat; 
+      texture.image = image;
+      texture.needsUpdate = true;
+      texture.minFilter = THREE.LinearFilter;
 
-  if(dashed) {
-          mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
-  } else {
-          mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+      if (textureCallback) {
+        textureCallback(texture);
+      }
+
+    } );
+      
+    var loader = new THREE.OBJLoader( manager );
+    loader.load( filename + '.obj', function ( object ) {
+
+      object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+          child.material.side = THREE.DoubleSide;
+          child.material.map = texture;
+        }
+      } );
+
+      if (objectCallback) {
+        objectCallback(object);
+      }
+
+    }, onProgress, onError );
   }
-
-  geom.vertices.push( src.clone() );
-  geom.vertices.push( dst.clone() );
-  geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
-
-  var axis = new THREE.Line( geom, mat, THREE.LinePieces );
-
-  return axis;
-
-}
+  
+};
