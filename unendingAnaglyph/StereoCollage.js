ecc1b46f476a6b114b6e0b8f26a52fd8b8e941d9
Erik Ulberg <ulberge@gmail.com>
2017-08-15 12:39:23 -0700

    Updates

--- unendingAnaglyph/StereoCollage.js
+++ unendingAnaglyph/StereoCollage.js
@@ -10,7 +10,7 @@
   const diff = 35;
   const rate = 0.4;
   const near = -diff*0.2;
-  const far = -diff*5.2;
+  const far = -diff*10.2;
   const STEREO_LAYERS = [
     {
       left: 'img/1_L2-01.png',
@@ -37,31 +37,31 @@
       right: 'img/990_R2-01.png',
       depth: diff*5
     },
-    // {
-    //   left: 'img/1_L2-01.png',
-    //   right: 'img/1_R2-01.png',
-    //   depth: diff*6
-    // },
-    // {
-    //   left: 'img/97_L2-01.png',
-    //   right: 'img/97_R2-01.png',
-    //   depth: diff*7
-    // },
-    // {
-    //   left: 'img/93_L2-01.png',
-    //   right: 'img/93_R2-01.png',
-    //   depth: diff*8
-    // },
-    // {
-    //   left: 'img/99_L2-01.png',
-    //   right: 'img/99_R2-01.png',
-    //   depth: diff*9
-    // },
-    // {
-    //   left: 'img/990_L2-01.png',
-    //   right: 'img/990_R2-01.png',
-    //   depth: diff*10
-    // },
+    {
+      left: 'img/1_L2-01.png',
+      right: 'img/1_R2-01.png',
+      depth: diff*6
+    },
+    {
+      left: 'img/97_L2-01.png',
+      right: 'img/97_R2-01.png',
+      depth: diff*7
+    },
+    {
+      left: 'img/93_L2-01.png',
+      right: 'img/93_R2-01.png',
+      depth: diff*8
+    },
+    {
+      left: 'img/99_L2-01.png',
+      right: 'img/99_R2-01.png',
+      depth: diff*9
+    },
+    {
+      left: 'img/990_L2-01.png',
+      right: 'img/990_R2-01.png',
+      depth: diff*10
+    },
     // {
     //   left: 'img/1_L_edit-01.png',
     //   right: 'img/1_R_edit-01.png',
@@ -150,7 +150,7 @@
 
     camera = new THREE.PerspectiveCamera( 60, 1.33, 0.1, 1000 );
     camera.position.z = 0;
-    camera.setFocalLength( 50 );
+    camera.setFocalLength( 30 );
 
     scene = new THREE.Scene();
     scene.fog = new THREE.FogExp2( 0xABCDEF, 0.001);
