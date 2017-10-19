1b47f93624c5cd8b57d2ffd49ea065ca35a0de1e
Erik Ulberg <ulberge@gmail.com>
2017-10-19 12:26:56 -0700

    update wc

--- app/Components/WCRemix.js
+++ app/Components/WCRemix.js
@@ -7,88 +7,76 @@ export default class extends Component {
   render() {
     return (
         <div className={classnames(className, 'container projectPage')}>
-          <h2>Transparent Collage, 2017</h2>
+          <h2>Watercolor Tech, 2017 (In Progress)</h2>
+          <h4>An adaptive system to let me focus on the good parts of watercolor.</h4>
           <div className="row">
             <div className="col-md-12">
               <h3>Project Description</h3>
             </div>
             <div className="col-md-5">
-              <p><b>KEYWORDS:</b> Machine Learning, Watercolor, <a href="http://www.wekinator.org/">Wekinator</a>, Processing</p>
-              <p>Watercolor is a difficult medium. Each painting has moments that are exceptional, but they tend to fall apart through the course of painting an entire scene. To circumvent this issue, I used digital collaging.</p>
-              <p>In order to make digital collages, I needed a program to convert digital images of watercolor into transparent versions approximating the pigment on the paper (without the white of the paper). I needed an algorithm to convert an opaque color into the most transparent color within a certain visual threshold.</p>
+              <p><b>KEYWORDS:</b> User-Centered Design, Augmented Reality Projection, Machine Learning, Watercolor, <a href="http://www.wekinator.org/">Wekinator</a>, Processing</p>
+              <p>A custom toolkit to make watercolor easier and allow me to focus on creating beautiful and expressive marks.</p>
+              <h4>Tools:</h4>
+              <ul><li><b>Analysis Program</b>: value studies, color studies, testing out light/medium/dark layers</li><li><b>Projection Program</b>: proportions, would it look better if it was darker?</li><li><b>Transparency Conversion Tool</b>: compositing pieces</li></ul>
             </div>
-            <div className="col-md-2 text-center" style={{ paddingTop: '60px' }}>
-              <img src="./imgs/wc_montage.png" />
-              <small>Paintings to collage</small>
-            </div>
-            <div className="col-md-1 hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '120px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></div>
             <div className="col-md-4 text-center" style={{ paddingTop: '0px' }}>
               <img src="./imgs/wc_composite.jpg" />
               <small>Collage</small>
             </div>
-          </div>
-          <div className="row text-center">
-            <h4>Transparency Conversion Tool</h4>
-            <div className="col-md-3 col-md-offset-2 text-center">
-                <img src="./imgs/wc_tool.jpg" className="" />
-                <small>Tool for converting images</small>
+            <div className="col-md-1 hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '120px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></div>
+            <div className="col-md-2 text-center" style={{ paddingTop: '60px' }}>
+              <img src="./imgs/wc_post.png" />
+              <small>Transparency conversion tool built with Machine Learning and Wekinator</small>
+              <div className=" hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '60px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></div>
             </div>
-            <div style={{ paddingTop: '80px' }}>
-              <div className="col-md-2 text-center">
-                  <img src="./imgs/wc_tree0.jpg" className="" />
-                  <small>Before</small>
-              </div>
-              <div className="col-md-1 hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '60px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></div>
-              <div className="col-md-2 text-center">
-                  <img src="./imgs/wc_tree_2.png" className="" style={{ border: '1px solid #333' }} />
-                  <small>After</small>
-              </div>
+
+          </div>
+          <div className="row">
+            <div className="col-md-2 col-md-offset-1">
+              <img src="./imgs/wc_source_composite.png" style={{ paddingTop: '20px' }} />
+              <small>Source photos</small>
+            </div>
+            <div className="col-md-1 hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '60px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></div>
+            <div className="col-md-2">
+              <img src="./imgs/wc_analysis.jpg" style={{ paddingTop: '30px' }} />
+              <small>Analysis program</small>
+            </div>
+            <div className="col-md-1 hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '60px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></div>
+            <div className="col-md-2">
+              <img src="./imgs/wc_proj.jpg" />
+              <small>Augmented Reality Projection/Camera setup</small>
+            </div>
+            <div className="col-md-1 hidden-xs hidden-sm" style={{ textAlign: 'center', paddingTop: '60px' }}><span style={{ fontSize: '30px' }} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></div>
+            <div className="col-md-2 text-center">
+              <img src="./imgs/wc_montage.png" />
+              <small>Watercolor paintings</small>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-8 col-md-offset-2">
-              <h3>Machine Learning</h3>
-            </div>
-            <div className="col-md-8 col-md-offset-2">
-              <p>The simple linear algorithm I tried did not work well and a brute force algorithm searching for a good transparent match was not performant. After testing various strategies, I trained a neural network to convert colors into transparent versions with a program I made using Processing and <a href="http://www.wekinator.org/">Wekinator</a>.</p>
-            </div>
-
-            <div className="col-md-8 col-md-offset-2 colorChart text-center">
-              <h4>Color Test Chart</h4>
-              <div className="withMargin">
-                <img src="./imgs/wc_chart2.jpg"/>
-                <small>Outcomes for the different algorithms at various saturation and brightness, with and without an image behind the chart.</small>
+            <div className="col-md-6">
+              <h4>What I want:</h4>
+              <div className="col-md-6">
+                <p>
+                  <img src="./imgs/wc_good_0.jpg" />
+                </p>
+                <img src="./imgs/wc_good_1.jpg" />
               </div>
-            </div>
-            <div className="col-md-12 text-center">
-              <h4>Processing Training Sketch</h4>
-              <div className="withMargin">
-                <img src="./imgs/wc_trainer.jpg"/>
-                <small>Processing sketch to send training examples to Wekinator.</small>
+              <div className="col-md-6">
+                <img src="./imgs/wc_good_2.jpg" />
               </div>
+              <small>Frugal use of bold marks with dynamic and meaningful shapes, pigment blending freely wet-in-wet to create transparent and spontaneous mixes</small>
             </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2">
-              <h3>Watercolor</h3>
-            </div>
-            <div className="col-md-8 col-md-offset-2">
-              <p>This method required some sensitivity to hard and soft edges. Before starting to make the sections, I did some planning and decided where the hard, dark edges would be and how the parts would layer.</p>
-            </div>
-            <div className="col-md-4 col-md-offset-2 colorChart text-center">
-              <div className="withMargin">
-                <img src="./imgs/wc_sketch2.jpg"/>
-                <small>Diagram showing sections. Lines represent a soft edge and arrows represent a hard edge.</small>
+            <div className="col-md-6">
+              <h4>What I don't want:</h4>
+              <div className="col-md-6">
+                <img src="./imgs/wc_bad_0.jpg" style={{ paddingBottom: '10px' }} />
+                <img src="./imgs/wc_bad_1.jpg" />
               </div>
-            </div>
-            <div className="col-md-4 text-center">
-              <div className="withMargin">
-                <img src="./imgs/wc_sketch0.jpg"/>
-                <small>Some planning for each section</small>
+              <div className="col-md-6">
+                <img src="./imgs/wc_bad_2.jpg" />
               </div>
+              <small>Fussiness, lack of contrast, boring colors mixed on the palette, focus on content over form</small>
             </div>
-
           </div>
         </div>
     );
