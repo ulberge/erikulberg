eff97b60207b2cbfe047fa9a3b3671ab42748bf5
Erik Ulberg <ulberge@gmail.com>
2017-10-26 09:35:31 -0700

    change wording

--- app/Components/WCRemix.js
+++ app/Components/WCRemix.js
@@ -14,7 +14,7 @@ export default class extends Component {
               <h3>Project Description</h3>
             </div>
             <div className="col-md-4">
-              <p><b>KEYWORDS:</b> Computer-Aided/User-Centered Design, Machine Learning, Augmented Reality Projection, Watercolor, <a href="http://www.wekinator.org/">Wekinator</a>, Processing</p>
+              <p><b>KEYWORDS:</b> User-Centered Design, Computer-Aided Watercolor, Machine Learning, Augmented Reality Projection, <a href="http://www.wekinator.org/">Wekinator</a>, Processing</p>
               <p>A custom toolkit to make watercolor easier and allow me to focus on creating beautiful and expressive marks.</p>
               <h4>Tools:</h4>
               <ul>
