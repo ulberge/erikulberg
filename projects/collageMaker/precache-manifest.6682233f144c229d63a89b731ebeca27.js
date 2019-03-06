38e736c5d42d060d36b41e040bdc1ad34bf2613c
Erik Ulberg <ulberge@gmail.com>
2019-03-06 14:19:34 -0500

    Updates

---
+++ projects/collageMaker/precache-manifest.6682233f144c229d63a89b731ebeca27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "a8de0abddf45e1673a99",
+    "url": "./static/js/main.a8de0abd.chunk.js"
+  },
+  {
+    "revision": "9036fd90fa5c0e5baeff",
+    "url": "./static/js/2.9036fd90.chunk.js"
+  },
+  {
+    "revision": "a8de0abddf45e1673a99",
+    "url": "./static/css/main.2e6d8d78.chunk.css"
+  },
+  {
+    "revision": "25161b8a0452d1888a586757827bd7c3",
+    "url": "./index.html"
+  }
+];
